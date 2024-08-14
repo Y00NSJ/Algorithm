1b515d9f1e157a0c11b2e46a359894505cdbccf9
Yoon Seo Jin <154308339+Y00NSJ@users.noreply.github.com>
2024-08-14 16:55:22 +0900

    [Bronze V] Title: 개수 세기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.cc
+++ 백준/Bronze/10807. 개수 세기/개수 세기.cc
@@ -2,16 +2,15 @@
 using namespace std;
 
 int main() {
-	int c;
-	cin >> c;
+	int c, i, t, a;
 	int arr[100];
+	cin >> c;
+	
 
-	int i;
 	for (i = 0; i < c; i++) {
 		cin >> arr[i];
 	}
 
-	int t, a;
 	a = 0;
 	cin >> t;
 	for (i = 0; i < c; i++) {
