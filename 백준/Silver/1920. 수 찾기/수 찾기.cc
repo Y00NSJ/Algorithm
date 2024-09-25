5a4bd7d3787ae590e3745418e5db507ec2cbd527
Yoon Seo Jin <154308339+Y00NSJ@users.noreply.github.com>
2024-09-25 12:52:44 +0900

    [Silver IV] Title: 수 찾기, Time: 44 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -2,21 +2,18 @@
 using namespace std;
 
 int lst[100001];
-int target[100001];
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int n, m;
+    int n, m, target;
     cin >> n;
     for (int i = 0; i < n; i++) cin >> lst[i];
     // 정렬!
     sort(lst, lst + n);
     cin >> m;
-    for (int i = 0; i < m; i++) cin >> target[i];
-
-    for (int i = 0; i < m; i++) {
-        int start = 0, end = n - 1;
-        cout << binary_search(lst, lst+n, target[i]) << '\n';
+    while (m--) {
+        cin >> target;
+        cout << binary_search(lst, lst + n, target) << '\n';
     }
 }
